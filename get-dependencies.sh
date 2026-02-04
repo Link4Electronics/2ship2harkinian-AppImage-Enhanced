a20a40ca79d8471f80a29225f9283e8c4e95f6fe
Link4Electronics <link4electronics@gmail.com>
2026-02-04 07:34:14 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,9 +15,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package
-make-aur-package ttf-fipps
-make-aur-package ttf-press-start-2p
 make-aur-package zenity-rs-bin
+make-aur-package
 
 # If the application needs to be manually built that has to be done down here
