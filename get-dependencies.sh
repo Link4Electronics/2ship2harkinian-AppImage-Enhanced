23a56344e1163a13136135513537a769d184742a
Link4Electronics <link4electronics@gmail.com>
2026-04-07 03:35:04 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,7 +7,15 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    libdecor \
+    cmake         \
+    fmt           \
+    libdecor      \
+    libzip        \
+    ninja         \
+    nlohmann-json \
+    sdl2          \
+    sdl2_net      \
+    spdlog        \
     tinyxml2
 
 echo "Installing debloated packages..."
@@ -16,6 +24,29 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 make-aur-package zenity-rs-bin
-make-aur-package
 
 # If the application needs to be manually built that has to be done down here
+echo "Making stable build of 2ship2harkinian..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/HarbourMasters/2ship2harkinian"
+VERSION="$(git ls-remote --tags --sort="v:refname" "$REPO" | tail -n1 | sed 's/.*\///; s/\^{}//')"
+git clone --branch "$VERSION" --single-branch --recursive --depth 1 "$REPO" ./2ship2harkinian
+echo "$VERSION" > ~/version
+
+mkdir -p ./AppDir/bin
+cd ./2ship2harkinian
+patch -Np1 -i ../spaghettikart-cmake-flags.patch
+cmake . \
+    -Bbuild \
+    -GNinja \
+    -DNON_PORTABLE=On
+cmake --build build --config Release
+cmake --build build --config Release --target GenerateO2R
+
+mv -v build/yamls ../AppDir/bin
+mv -v build/meta ../AppDir/bin
+mv -v build/Spaghettify ../AppDir/bin
+mv -v build/config.yml ../AppDir/bin
+mv -v build/spaghetti.o2r ../AppDir/bin
+wget -O ../AppDir/bin/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
+cp -rv icon.png /usr/share/pixmaps/spaghettikart.png
