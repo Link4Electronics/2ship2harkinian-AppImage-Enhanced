43c8b4d7cb65485d8c5cda19c35df5c7bab26856
Link4Electronics <link4electronics@gmail.com>
2026-04-07 05:01:47 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,14 +8,10 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/pixmaps/2s2h.png
-export DESKTOP=/usr/share/applications/2s2h.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/2s2h /usr/bin/2s2h-otr-exporter /usr/bin/zenity
-mv /opt/2s2h/2ship.o2r ./AppDir/bin
-mv /opt/2s2h/gamecontrollerdb.txt ./AppDir/bin
+quick-sharun ./AppDir/bin/* /usr/bin/zenity
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
