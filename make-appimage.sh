e945927504e8f66a0d0a400367486bf3568eac8a
Link4Electronics <link4electronics@gmail.com>
2026-08-24 08:24:50 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,6 +7,9 @@ export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=https://raw.githubusercontent.com/HarbourMasters/2ship2harkinian/refs/heads/develop/mm/linux/2s2hIcon.png
+export DESKTOP=https://raw.githubusercontent.com/HarbourMasters/2ship2harkinian/refs/heads/develop/mm/linux/2s2h.desktop
+export STARTUPWMCLASS=2s2h.elf
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
